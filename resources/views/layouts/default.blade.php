e803d56f5f0be76e3af07a929775497dcffa2a1e
subing008 <275120420@qq.com>
2018-01-26 17:32:07 +0800

    add style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,24 @@
 <html>
   <head>
     <title>@yield('title','Sample')</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
+  <header class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+          <a href="{{route('home')}}" id="logo">Sample App</a>
+          <nav>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="{{route('help')}}">帮助</a></li>
+              <li><a href="#">登录</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <div class="contrainer">
     @yield('content')
+    </div>
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.default')
 @section('content')
-<h1>主页</h1>
+<div class="container">
+    <div class="jumbotron">
+        <h1 class="display-3">Jumbo heading</h1>
+        <p class="lead">Jumbo helper text</p>
+        <hr class="my-2">
+        <p>More info</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+        </p>
+    </div>
+</div>
 @stop
